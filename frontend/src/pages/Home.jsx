ac501f9efa5ae81853ce571da449039fffcd704f
Alex Burrows <burrows.alexj@gmail.com>
2024-11-28 15:11:15 -0500

    create form to make notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,9 +24,9 @@ function Home() {
             .then((res) => {
                 if (res.status === 204) alert("Note was deleted")
                 else alert("Failed to delete note")
+                getNotes()
             })
             .catch((error) => alert(error))
-        getNotes()
     };
 
     const createNote = (e) => {
@@ -36,12 +36,45 @@ function Home() {
             .then((res) => {
                 if (res.status === 201) alert("Note created")
                 else alert("Failed to make note")
+                getNotes();
+
             })
             .catch((error) => alert(error))
-        getNotes();
     }
 
-    return <div>Home</div>;
+    return <div>
+        <div>
+            <h2>Notes</h2>
+
+        </div>
+        <h2>Create a Notes</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+            <br/>
+            <input 
+                type="text"
+                id="title" 
+                name="title" 
+                required 
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+
+            <label htmlFor="content">Content:</label>
+            <br />
+            <textarea 
+                id="content" 
+                name="content" 
+                required 
+                value={content} 
+                onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+            <br />
+
+            <input type="submit" value="Submit"></input>
+
+        </form>
+    </div>;
 }
 
 export default Home;
