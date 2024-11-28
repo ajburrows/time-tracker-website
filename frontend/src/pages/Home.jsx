74599609c7ac07a3d7966839cd3a80dc42979f4e
Alex Burrows <burrows.alexj@gmail.com>
2024-11-28 15:01:56 -0500

    write methods to create, get, and delete notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,47 @@
+import { useState, useEffect } from "react"
+import api from "../api"
+
 function Home() {
-    return <div>Home</div>
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    const getNotes = () => {
+        api
+        .get("/api/notes/")
+        .then((res) => res.data)
+        .then((data) => { setNotes(data); console.log(data) })
+        .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Note was deleted")
+                else alert("Failed to delete note")
+            })
+            .catch((error) => alert(error))
+        getNotes()
+    };
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api
+            .post("/api/notes/", {content, title})
+            .then((res) => {
+                if (res.status === 201) alert("Note created")
+                else alert("Failed to make note")
+            })
+            .catch((error) => alert(error))
+        getNotes();
+    }
+
+    return <div>Home</div>;
 }
 
-export default Home
+export default Home;
